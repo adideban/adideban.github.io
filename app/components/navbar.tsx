221699690f0ce44433a988e1a87b1216b8e829db
Seena Pourzand <seenapourzand@gmail.com>
2024-10-04 23:24:41 -0700

    small fix for deploy

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './Navbar.module.css';
+import styles from './navbar.module.css';
 
 const Navbar = () => {
   const navItems = [
